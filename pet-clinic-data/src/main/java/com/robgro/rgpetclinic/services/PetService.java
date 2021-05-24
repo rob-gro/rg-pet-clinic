c9eb15c0a9368e78ca38fb959fc52df10479ba24
RobGro <r.grodzki74@gmail.com>
2021-05-24 20:41:46 +0100

    refactoring to common CRUD interface. Closes #12

--- pet-clinic-data/src/main/java/com/robgro/rgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/robgro/rgpetclinic/services/PetService.java
@@ -2,11 +2,6 @@ package com.robgro.rgpetclinic.services;
 
 import com.robgro.rgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
