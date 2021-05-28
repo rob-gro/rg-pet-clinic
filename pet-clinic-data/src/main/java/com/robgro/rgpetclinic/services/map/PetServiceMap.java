c0af746a6b72222c90e71eb935061f1199413dc2
RobGro <r.grodzki74@gmail.com>
2021-05-28 22:02:30 +0100

    added bootstrap for data. Closes #15

--- pet-clinic-data/src/main/java/com/robgro/rgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/robgro/rgpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.robgro.rgpetclinic.services.map;
 
 import com.robgro.rgpetclinic.model.Pet;
-import com.robgro.rgpetclinic.services.CrudService;
+import com.robgro.rgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/com/robgro/rgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/robgro/rgpetclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.robgro.rgpetclinic.services.map;
 
 import com.robgro.rgpetclinic.model.Vet;
-import com.robgro.rgpetclinic.services.CrudService;
+import com.robgro.rgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
