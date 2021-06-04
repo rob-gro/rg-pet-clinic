75d5ff89222393ff881796250a532096a527fbc7
RobGro <r.grodzki74@gmail.com>
2021-06-04 22:34:58 +0100

    added Specialize.java, added to Vet. Closes #28

---
+++ pet-clinic-data/src/main/java/com/robgro/rgpetclinic/model/Specialize.java
@@ -0,0 +1,14 @@
+package com.robgro.rgpetclinic.model;
+
+public class Specialize extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/robgro/rgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/robgro/rgpetclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package com.robgro.rgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<Specialize> specializes;
+
+    public Set<Specialize> getSpecializes() {
+        return specializes;
+    }
+
+    public void setSpecializes(Set<Specialize> specializes) {
+        this.specializes = specializes;
+    }
 }
