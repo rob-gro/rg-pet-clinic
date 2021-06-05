249b4fb2467540fe592c762dd6f98026a6df6ae3
RobGro <r.grodzki74@gmail.com>
2021-06-05 17:51:54 +0100

    fixed Error. Closes #46

--- pet-clinic-web/src/main/java/com/robgro/rgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/robgro/rgpetclinic/controllers/OwnerController.java
@@ -15,11 +15,16 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html", "/find"})
+    @RequestMapping({"", "/", "/index", "/index.html", "/ffind"})
     public String listOwners(Model model) {
 
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
+    }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notimplemented";
     }
 }
