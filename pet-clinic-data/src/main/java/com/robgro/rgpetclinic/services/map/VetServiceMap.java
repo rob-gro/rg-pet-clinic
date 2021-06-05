227cad43d89af0af74643e157825a91ecb1261d5
RobGro <r.grodzki74@gmail.com>
2021-06-05 15:13:14 +0100

    added Specializes to Vets on startup. Closes #35

--- pet-clinic-data/src/main/java/com/robgro/rgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/robgro/rgpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.robgro.rgpetclinic.services.map;
 
+import com.robgro.rgpetclinic.model.Specialize;
 import com.robgro.rgpetclinic.model.Vet;
+import com.robgro.rgpetclinic.services.SpecializeService;
 import com.robgro.rgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecializeService specializeService;
+
+    public VetServiceMap(SpecializeService specializeService) {
+        this.specializeService = specializeService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +29,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+
+        if (object.getSpecializes().size() > 0) {
+            object.getSpecializes().forEach(specialize -> {
+                if (specialize.getId() == null) {
+                    Specialize savedSpecialize = specializeService.save(specialize);
+                    specialize.setId(savedSpecialize.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
--- pet-clinic-web/src/main/java/com/robgro/rgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/robgro/rgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package com.robgro.rgpetclinic.bootstrap;
 
-import com.robgro.rgpetclinic.model.Owner;
-import com.robgro.rgpetclinic.model.Pet;
-import com.robgro.rgpetclinic.model.PetType;
-import com.robgro.rgpetclinic.model.Vet;
+import com.robgro.rgpetclinic.model.*;
 import com.robgro.rgpetclinic.services.OwnerService;
 import com.robgro.rgpetclinic.services.PetTypeService;
+import com.robgro.rgpetclinic.services.SpecializeService;
 import com.robgro.rgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,16 +16,27 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecializeService specializeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+                      SpecializeService specializeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specializeService = specializeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
 
+        int count = petTypeService.findAll().size();
+
+        if (count == 0) {
+            loadData();
+        }
+    }
+
+    private void loadData() {
         PetType dog = new PetType();
         dog.setName("Dog");
         PetType saveDogPetType = petTypeService.save(dog);
@@ -35,6 +44,18 @@ public class DataLoader implements CommandLineRunner {
         PetType cat = new PetType();
         cat.setName("Cat");
         PetType saveCatPetType = petTypeService.save(cat);
+
+        Specialize radiology = new Specialize();
+        radiology.setDescription("Radiology");
+        Specialize savedRadiology = specializeService.save(radiology);
+
+        Specialize surgery = new Specialize();
+        surgery.setDescription("Surgery");
+        Specialize savedSurgery = specializeService.save(surgery);
+
+        Specialize dentistry = new Specialize();
+        dentistry.setDescription("Dentistry");
+        Specialize savedDentistry = specializeService.save(dentistry);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
@@ -73,11 +94,15 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecializes().add(savedRadiology);
+
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+        vet2.getSpecializes().add(savedSurgery);
+
         vetService.save(vet2);
 
         System.out.println("Loaded Vets....");
