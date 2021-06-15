6367fcf55117759466465bf5a23625fda0727f3e
RobGro <r.grodzki74@gmail.com>
2021-06-15 13:37:39 +0100

    updated Owner with JPA entity mappings. Closes #37

--- pet-clinic-data/src/main/java/com/robgro/rgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/robgro/rgpetclinic/model/BaseEntity.java
@@ -17,10 +17,6 @@ public class BaseEntity implements Serializable {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public boolean isNew() {
         return this.id == null;
     }
